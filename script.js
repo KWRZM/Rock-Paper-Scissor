9253c556fb7639ac9d5a18c8ecb304457c895c1b
KWRZM <84276353+KWRZM@users.noreply.github.com>
2023-02-07 08:33:47 +0100

    First Version
    First Version

---
+++ script.js
@@ -0,0 +1,67 @@
+function getComputerChoice(){
+    let choice= Math.floor(Math.random() * 3) + 1;
+    switch(choice){
+        case 1: return "rock"
+        case 2: return "paper"
+        case 3: return "scissor"
+    }
+}
+
+function palyRound(playerSelection,computerSelection){  // if player win return 1
+    let j,i;                                            // if computer win return -1
+    const possib=["scissor","paper","rock"];            // if null return 0
+    
+    // Debug prints ##################################
+    console.log("Computer choice: "+computerSelection);
+    console.log("Your choice: "+playerSelection);
+    // ###############################################
+
+    for(i=0;i<3;i++){
+        if(possib[i]===playerSelection){break;}
+    }
+    if(i==2){j=0;}
+    else{j=i+1;}
+    
+    if(possib[i]===computerSelection){ return 0}
+    else if(possib[j]===computerSelection){ return 1}
+    else{ return -1}
+}
+
+function visualResult(value){
+    switch(value){
+        case 0:console.log("Match null");return "Match null";break;
+        case 1:console.log("You won!");return;break;
+        case -1:console.log("You lost...");return;break;
+    }
+}
+
+function retResult(value){
+    switch(value){
+        case  0: return "Match null"
+        case  1: return "You won!"
+        case -1: return "You lost..."
+    }
+}
+
+function getPlayerChoice(){
+    let myChoice;
+    do{
+        myChoice=prompt("What's your choice? (Paper,Scissor,Rock)");
+        myChoice=myChoice.toLowerCase();
+    }while(myChoice!="rock" && myChoice!="paper" && myChoice!="scissor");
+    return myChoice
+}
+
+function game(rounds){
+    let T=[],i;
+    for(let i=0;i<rounds;i++){
+        T[i]=palyRound(getPlayerChoice(),getComputerChoice());
+    }
+    console.log("-Result display:");
+    for(i=0;i<rounds;i++){
+        console.log(retResult(T[i])+"\t(Round "+i+")");
+    }
+}
+
+const btn = document.querySelector('#btn');
+btn.onclick = () => game(prompt("How many games do you want to play?"));
